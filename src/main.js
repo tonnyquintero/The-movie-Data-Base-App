ae5a361a88b276afe2f6c4f13596869de1ee03e1
tonny187 <tonyoanthony@gmail.com>
2022-11-06 13:18:10 -0500

    Building all the html and css for the project

--- src/main.js
+++ src/main.js
@@ -1,1 +1,26 @@
-console.log('Hello world');
+import {API_KEY} from "./secret.js";
+
+async function getTrendingMoviesPreview() {
+    const res = await fetch('https://api.themoviedb.org/3/trending/tv/day?api_key=' + API_KEY)
+    const data = await res.json();
+    const series = data.results;
+
+    series.forEach(serie => {
+        const trendingPreviewMoviesContainer = document.querySelector
+        ('#trendingPreview .trendingPreview-movieList')
+
+        const movieContainer = document.createElement('div')
+        movieContainer.classList.add('movie-container')
+
+        const movieImg = document.createElement('img')
+        movieImg.classList.add('movie-img');
+        movieImg.setAttribute('alt', serie.title);
+        movieImg.setAttribute('src', 'https://image.tmdb.org/t/p/w300'
+        + serie.poster_path);
+
+        movieContainer.appendChild(movieImg)
+        trendingPreviewMoviesContainer.appendChild(movieContainer)
+    });
+}
+
+getTrendingMoviesPreview();
